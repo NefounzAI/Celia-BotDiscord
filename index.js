a0d276ddb0529424900c599b95938d94be7826d9
Rifqiii <rifqibkl14@gmail.com>
2024-10-11 11:05:15 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
