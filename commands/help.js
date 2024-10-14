83418b9b794c5a6520ce54b75be35a8cf3f04d95
Rifqiii <rifqibkl14@gmail.com>
2024-10-14 12:57:53 +0700

    menambahkan icon di title dan mengubah tampilann help

--- commands/help.js
+++ commands/help.js
@@ -15,7 +15,10 @@ module.exports = {
                 { name: '!ping', value: 'Menampilkan latensi bot.' },
                 { name: '!nsfw', value: 'Menampilkan konten nsfw.' },
             )
-            .setFooter({ text: 'Gunakan perintah dengan prefix "!".' });
+            .setFooter({ text: 'Gunakan perintah dengan prefix "!".' })
+            .setTimestamp() // Menambahkan timestamp
+            .setThumbnail('https://i.pinimg.com/736x/47/f4/64/47f464522a1a1a9a534b7936171a8796.jpg') // Ganti dengan URL thumbnail yang sesuai
+            .setAuthor({ name: 'Nefounz', url: 'https://nefounz.my.id', iconURL: 'https://i.pinimg.com/736x/bb/5c/14/bb5c14a9533d80e8d08ebdc631b6d062.jpg' }); // Ganti dengan URL icon yang sesuai
 
         message.channel.send({ embeds: [embed] });
     }
